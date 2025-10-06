efa6d6dc5fbe9a34de4c2ac0c0afce58d4471aad
Enzo-Silveira <enzoleranhe@gmail.com>
2025-10-05 21:25:35 -0300

    Minigame defeituoso
    tem grid mas ta tudo errado.

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
+}
